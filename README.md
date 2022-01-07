cac00dcf4ed714f410464f7b52e00f21da607afd
Emmanuel Bosquet <bjokac@gmail.com>
2022-01-07 17:20:25 +0100

    readme

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # The basics of unix sockets
 
 This repository serves as a reference for [this tutorial blogpost](emmanuelbosquet.com)
+
+## How to run
+
+[Install Rust and Cargo](https://www.rust-lang.org/tools/install),
+and then do:
+
+    rm mysocket
+    cargo run --bin server
+
+And in a separate terminal, run the client:
+
+    cargo run --bin client
+
+If all is well,
+
+-   the hello message should display on the server side
+-   the "I hear you" response should display on the client side
+
+You can run the client as many times as you want, since the server runs in a loop.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
-use std::os::unix::net::{UnixListener, UnixStream};
-
-use anyhow::Context;
-
-fn main() -> anyhow::Result<()> {
-    let socket_path = "mysocket";
-
-    if std::fs::metadata(socket_path).is_ok() {
-        println!("A socket is already present. Deleting...");
-        std::fs::remove_file(socket_path)
-            .with_context(|| format!("could not delete previous socket at {:?}", socket_path))?;
-    }
-
-    let unix_listener =
-        UnixListener::bind(socket_path).context("Could not create the unix socket")?;
-
-    let (unix_stream, socket_address) = unix_listener
-        .accept()
-        .context("Failed at accepting a connection on the unix listener")?;
-
-    Ok(())
+fn main() {
+    println!("Hello, World!");
+    println!("This is not the executable you're looking for");
 }
